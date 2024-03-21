83b01a39478fa24fb60954903b02495c8a2445e7
NanaOdamme <eronastyles@gmail.com>
2024-03-21 13:13:02 +0000

    updated

--- app2.js
+++ app2.js
@@ -1,3 +1,45 @@
+const portfolioHeader={
+template: `
+<section class="hero containder-md text-center pt-5">
+        
+<nav class="navbar navbar-expand-lg shadow fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand px-3" href="#home"><h2>{{ appName }}</h2></a>
+     
+        <div class="navbar-nav px-3 ">
+        <a class="nav-link" aria-current="page" href="index.html" @click.prevent="goBack">Go Back</a>
+          
+        </div>
+      </div>
+    
+  </nav>
+
+  <video autoplay loop muted plays-line class="background-clip">
+  <source :src="videoSource" type="video/mp4">
+</video>
+<div class="content">
+
+
+<h1 class="h1 text-white mb-5">{{ pageTitle }}</h1>
+        <a :href="githubLink" class="btn mb-5 border border-2 border-warning rounded-5">{{ githubButtonText }}</a>
+      </div>
+</section>
+`,
+data() {
+  return {
+    appName: 'NanA',
+    pageTitle: 'Explore my work',
+    githubButtonText: 'Check my github',
+    githubLink: 'https://github.com/NanaOdamme',
+    videoSource: 'images/vid.mp4'
+  };
+},
+methods: {
+  goBack() {
+    window.location.href = 'index.html';
+  }
+}
+}
 const Filters = {
     template:`
    
@@ -119,5 +161,6 @@ const Filters = {
 
     const app2 = Vue.createApp({
     });
-    app2.component('image-filters', Filters)
+    app2.component('portfolio-header', portfolioHeader);
+    app2.component('image-filters', Filters);
     app2.mount('#app2');
